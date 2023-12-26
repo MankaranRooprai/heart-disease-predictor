d06d9ed6a6b287af9e8743b6c858903cd3b6104f
Mankaran Rooprai <mankaranrooprai@gmail.com>
2023-12-26 17:02:24 -0500

    Ensure data is valid.

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ import seaborn as sns
 import os
 import warnings
 
-print(os.listdir())
-warnings.filterwarnings('ignore')
+dataset = pd.read_csv("heart.csv")
+
+# print out the dataset type (should be dataframe), the dataset shape (number of rows and columns), and the first 5 rows
+print(type(dataset))
+print(dataset.shape)
+print(dataset.head(5))
+
+# print 5 random rows and describe the data
+print(dataset.sample(5))
+print(dataset.describe())
