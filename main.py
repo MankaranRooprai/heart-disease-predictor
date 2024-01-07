16bd8ed0b0f5794bec0c2eb94804d06d8a4ba8ad
Mankaran Rooprai <mankaranrooprai@gmail.com>
2024-01-06 22:06:50 -0500

    Determine Random Forest, XGBoost, Neural Network accuracy scores.

--- main.py
+++ main.py
@@ -200,30 +200,30 @@ score_dt = round(accuracy_score(Y_pred_dt,Y_test)*100,2)
 print("The accuracy score achieved using Decision Tree is: "+str(score_dt)+" %")
 print("\n")
 
-# # calculate Random Forest accuracy score
-# max_accuracy = 0
-#
-# for x in range(2000):
-#     rf = RandomForestClassifier(random_state=x)
-#     rf.fit(X_train,Y_train)
-#     Y_pred_rf = rf.predict(X_test)
-#     current_accuracy = round(accuracy_score(Y_pred_rf,Y_test)*100,2)
-#     if(current_accuracy>max_accuracy):
-#         max_accuracy = current_accuracy
-#         best_x = x
-#
-# print(max_accuracy)
-# print(best_x)
-#
-# rf = RandomForestClassifier(random_state=best_x)
-# rf.fit(X_train,Y_train)
-# Y_pred_rf = rf.predict(X_test)
-#
-# print(Y_pred_rf.shape)
-#
-# score_rf = round(accuracy_score(Y_pred_rf,Y_test)*100,2)
-# print("The accuracy score achieved using Random Forest is: "+str(score_rf)+" %")
-# print("\n")
+# calculate Random Forest accuracy score
+max_accuracy = 0
+
+for x in range(2000):
+    rf = RandomForestClassifier(random_state=x)
+    rf.fit(X_train,Y_train)
+    Y_pred_rf = rf.predict(X_test)
+    current_accuracy = round(accuracy_score(Y_pred_rf,Y_test)*100,2)
+    if(current_accuracy>max_accuracy):
+        max_accuracy = current_accuracy
+        best_x = x
+
+print(max_accuracy)
+print(best_x)
+
+rf = RandomForestClassifier(random_state=best_x)
+rf.fit(X_train,Y_train)
+Y_pred_rf = rf.predict(X_test)
+
+print(Y_pred_rf.shape)
+
+score_rf = round(accuracy_score(Y_pred_rf,Y_test)*100,2)
+print("The accuracy score achieved using Random Forest is: "+str(score_rf)+" %")
+print("\n")
 
 # calculate XGBoost accuracy score
 xgb_model = xgb.XGBClassifier(objective="binary:logistic", random_state=42)
@@ -253,3 +253,6 @@ Y_pred_nn = rounded
 score_nn = round(accuracy_score(Y_pred_nn,Y_test)*100,2)
 print("The accuracy score achieved using Neural Network is: "+str(score_nn)+" %")
 #Note: Accuracy of 85% can be achieved on the test set, by setting epochs=2000, and number of nodes = 11.
+
+
+
